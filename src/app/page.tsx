f8c4a18d89f2e72baa5373693e989cd01aa24600
AndresMB <bmejiadeveloper2@gmail.com>
2024-02-21 21:09:56 -0500

    latets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,15 +28,14 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex flex-col items-center gap-y-2">
+        <h1 className="text-6xl text-black">
+          Quote Ranker
+          </h1>
+          <h2 className="text-4xl">
+            Claim your quote and rank it
+          </h2>
+          <input type='text' className="border border-black bg-transparent focus:bg-white shadow-inner drop-shadow-md text-4xl p-4 w-[500px] text-center" placeholder='"What do you have to say?"' />
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
